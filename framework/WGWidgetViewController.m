824f77cb735558cc7bf4736cebdd3c0fad1a42f6
Peter Hajas <peterhajas@gmail.com>
2011-03-22 01:27:18 -0400

    Framework now builds and deploys to the correct location. You still need to copy the headers into your sysroot include path, but this will change.

---
+++ framework/WGWidgetViewController.m
@@ -0,0 +1,20 @@
+#import "WGWidgetViewController.h"
+
+@implementation WGWidgetViewController
+
+-(NSString *)widgetName
+{
+    return @"An awesome Widge Widget!";
+}
+
+-(int)iconRowsWide
+{
+    return 2;
+}
+
+-(int)iconColumnsWide
+{
+    return 2;
+}
+
+@end
