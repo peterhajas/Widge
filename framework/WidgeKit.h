0686807455df0430fea0e6c15ae4aa50377190a8
Peter Hajas <peterhajas@gmail.com>
2011-04-30 09:58:47 -0400

    Relicensed WidgeKit under the LGPL.

--- framework/WidgeKit.h
+++ framework/WidgeKit.h
@@ -1,1 +1,9 @@
+/*
+Copyright (C) 2010-2011 Peter Hajas, Dustin Howett
+
+This work is licensed under the LGPL.
+
+Please see the included License file in the framework directory.
+*/
+
 #import <WidgeKit/WGWidgetViewController.h>
